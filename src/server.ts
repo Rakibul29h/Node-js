adcc069151df8c996f0d200b3832c937ec91c9fd
Rakibul29h <rakibul2170@gmail.com>
2026-06-27 22:13:39 +0600

    Simple route with method and root

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,14 @@
-import { createServer, IncomingMessage, Server } from "http";
+import { createServer, IncomingMessage, Server, ServerResponse } from "http";
 
-const server : Server= createServer((req:IncomingMessage,res)=>{
+const server : Server= createServer((req:IncomingMessage,res:ServerResponse)=>{
 
+    const url = req.url;
+    const method=req.method;
+
+    if(url ==='/' && method=="GET"){
+      res.writeHead(200,{"content-type":"text/plain"})
+      res.end("This is root route")
+    }
 })
 
 server.listen(3000,()=>{
