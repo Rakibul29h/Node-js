ace83f54cf19c5b9287809bfa425883b7f59e0d5
Rakibul29h <rakibul2170@gmail.com>
2026-06-27 22:21:38 +0600

    application /json header file handle and product route create

--- src/server.ts
+++ src/server.ts
@@ -6,8 +6,15 @@ const server : Server= createServer((req:IncomingMessage,res:ServerResponse)=>{
     const method=req.method;
 
     if(url ==='/' && method=="GET"){
-      res.writeHead(200,{"content-type":"text/plain"})
-      res.end("This is root route")
+      res.writeHead(200,{"content-type":"application/json"})
+      res.end(JSON.stringify({message:"This is root route"}))
+    }else if(url?.startsWith("/product")){
+        res.writeHead(200,{"content-type":"application/json"})
+      res.end(JSON.stringify({message:"This is Product route"}))
+    }
+    else{
+        res.writeHead(404,{"content-type":"application/json"})
+      res.end(JSON.stringify({message:"Route not found"}))
     }
 })
 
