ab6733df648ce3c8ac873161631c59fc60a9534f
Rakibul29h <rakibul2170@gmail.com>
2026-06-28 13:15:52 +0600

    Retrive single  Product with file handler

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,5 +1,6 @@
 import type { IncomingMessage, ServerResponse } from "node:http";
 import { readProduct } from "../service/product.service.js";
+import type { IProduct } from "../types/productsType.js";
 
 export const productController = (
   req: IncomingMessage,
@@ -7,15 +8,30 @@ export const productController = (
 ) => {
   const url = req.url;
   const method = req.method;
+  const urlParts = url?.split("/");
+  const id =
+    urlParts && urlParts[1] === "products" ? Number(urlParts[2]) : null;
 
-  if (url == "/product" && method == "GET") {
-
-   const products= readProduct();
-
+  // get all products
+  if (url == "/products" && method == "GET") {
+    const products = readProduct();
     res.writeHead(200, { "content-type": "application/json" });
-    res.end(JSON.stringify({ message: "Product retrives successfully" ,
-        data:products
-    }));
+    res.end(
+      JSON.stringify({
+        message: "Product retrives successfully",
+        data: products,
+      }),
+    );
+  } else if (method == "GET" && id) {
+    const products = readProduct();
+    const product = products.find((p: IProduct) => p.id == id);
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "Product retrives successfully",
+        data: product,
+      }),
+    );
   } else {
     res.writeHead(200, { "content-type": "application/json" });
     res.end(JSON.stringify({ message: "This is Product route but not found" }));
